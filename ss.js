960af63896a4ff8f8d5bbfbee7aafd97028f8001
Chris <clissmanux@gmail.com>
2020-04-03 00:17:19 -0600

    Add columns, add unique word count, format css

--- ss.js
+++ ss.js
@@ -3,18 +3,41 @@
 
 function organizeText() {
     let paragraphs = document.getElementById('paragraphInput').value
-    let arr = (Array.from(new Set(paragraphs.toLowerCase().replace(/[^\w\s]/g, "").replace(/\n/g, " ").split(' '))).sort().sort((a, b) => a.length - b.length))
+
+    let uniqueWords = 0;
     
+   
+    let arr = paragraphs.toLowerCase()
+        .replace(/\n/g, " ")
+        .replace(/[^\w\s\'-]/g, "")
+        .split(' ')
+        .sort()
+        .sort((a, b) => a.length - b.length)
+        .filter((x, i, s) => s.indexOf(x) == i && x.length > 0)
+
+        uniqueWords = arr.length
+
+
     for (let i = 0, p = 0; i < arr.length; i++) {
-        if (arr[i].length > p) {
-            p = arr[i].length;
+
+
+        let length = arr[i].length
+        if (length > p) {
+            p = length;
             arr.splice(i, 0,`</ol></li><li><span>${p}</span><ol>`);
         }
     }
 
+    
+    
     let words = arr.map(w => {
         return `<li>${w}</li>`
     })
+    
+    document.getElementById('info').innerHTML = `Unique Words: ${uniqueWords}`
+
+
+   
     
     document.getElementById('wordList').innerHTML = words.join('')
 }
